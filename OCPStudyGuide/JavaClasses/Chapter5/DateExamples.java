6f1f509dfbf90b70d6c80c246da34d51795b38c8
Ruben Dario <noiz23@gmail.com>
2019-09-26 20:02:38 -0300

    Accounting for Daylight Savings Time
    251

--- OCPStudyGuide/JavaClasses/Chapter5/DateExamples.java
+++ OCPStudyGuide/JavaClasses/Chapter5/DateExamples.java
@@ -1,6 +1,10 @@
 import java.time.*;
+import java.time.temporal.*;
 public class DateExamples{
     public static void main(String... dd){
+        
+        Instant now = Instant.now();
+/*        
         System.out.println(LocalDate.now());
         System.out.println(LocalTime.now());
         System.out.println(LocalDateTime.now());
@@ -17,6 +21,30 @@ public class DateExamples{
          System.out.println("Period: " + (Period.of(1, 20, 47)));
          
          System.out.println("--> " + Period.ofWeeks(3));
+
+        Instant later = Instant.now();
+        
+        Duration duration = Duration.between(now, later);
+        System.out.println("duration: " + duration.toMillis());
+
+        System.out.println("Instance: " + now );
+
+        Instant nextDay = now.plus(1, ChronoUnit.DAYS);
+        System.out.println("nextDay " + nextDay); 
+*/
+    LocalDate date = LocalDate.of(2016, Month.MARCH, 13);
+    
+    LocalTime time = LocalTime.of(1, 30);
+    
+    ZoneId zone = ZoneId.of("US/Eastern");
+    
+    ZonedDateTime dateTime = ZonedDateTime.of(date, time, zone);
+    
+    System.out.println("A- :" + dateTime); // 2016–03–13T01:30–05:00[US/Eastern]
+    
+    dateTime = dateTime.plusHours(1);
+    
+    System.out.println("B- :" + dateTime); // 2016–03–13T03:30–04:00[US/Eastern]
 
 
 
