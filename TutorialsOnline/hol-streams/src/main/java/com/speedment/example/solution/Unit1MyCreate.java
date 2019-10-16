3511b63dfb83fc27f3f1123c18fd485dec06d568
Ruben Dario <noiz23@gmail.com>
2019-10-16 17:47:14 -0300

    Continue part 2
    https://dzone.com/articles/become-a-master-of-java-streams-part-1-creating-st

---
+++ TutorialsOnline/hol-streams/src/main/java/com/speedment/example/solution/Unit1MyCreate.java
@@ -0,0 +1,55 @@
+package com.speedment.example.solution;
+
+import com.speedment.example.unit.Unit1Create;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public final class Unit1MyCreate implements Unit1Create {
+
+    @Override
+    public Stream<String> newStreamOfAToC() {
+        return Stream.of("A","B","C");
+    }
+
+    @Override
+    public IntStream newIntStreamOfOneToSeven() {
+        return IntStream.rangeClosed(1, 7);
+    }
+
+    @Override
+    public Stream<String> from(String[] array) {
+        return Stream.of(array);
+    }
+
+    @Override
+    public Stream<String> from(Collection<String> collection) {
+        return collection.stream();
+    }
+
+    @Override
+    public IntStream from(String s) {
+        return s.chars();
+    }
+
+    @Override
+    public IntStream infiniteAlternating() {
+        return IntStream.iterate(1, f -> f*-1);
+    }
+
+    @Override
+    public IntStream infiniteRandomInts(Random rnd) {
+        return rnd.ints();
+    }
+
+    @Override
+    public Stream<String> linesFromPoemTxtFile() throws IOException {
+        return Files.lines(Paths.get(FILE_NAME));
+    }
+}
