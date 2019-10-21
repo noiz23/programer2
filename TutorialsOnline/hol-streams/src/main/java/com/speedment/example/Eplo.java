42275400ba09c7e8363ee3369fa8ef0e612a3dc4
Ruben Dario <ruben.echeverri@axity.com>
2019-10-21 17:28:07 -0300

    Test from command line

--- TutorialsOnline/hol-streams/src/main/java/com/speedment/example/Eplo.java
+++ TutorialsOnline/hol-streams/src/main/java/com/speedment/example/Eplo.java
@@ -3,4 +3,17 @@ package com.speedment.example;
 public class Eplo {
 	private boolean b1;
 	private Boolean b2;
+	
+	public boolean isB1() {
+		return b1;
+	}
+	public void setB1(boolean b1) {
+		this.b1 = b1;
+	}
+	public Boolean getB2() {
+		return b2;
+	}
+	public void setB2(Boolean b2) {
+		this.b2 = b2;
+	}
 }
