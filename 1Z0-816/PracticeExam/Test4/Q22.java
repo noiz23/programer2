17aa44fa726116c1a386d860143d0c17e77779cb
Ruben Dario <ruben.echeverri@axity.com>
2019-11-11 16:43:23 -0300

    Continuar Q41 Test4

---
+++ 1Z0-816/PracticeExam/Test4/Q22.java
@@ -0,0 +1,11 @@
+import java.util.function.*;
+
+public class Q22{
+    public static void main(String... args){
+        //Predicate<Integer> p1 = (int i) -> i%2==0;
+        //Predicate p2 = (Integer i) -> i%2==0;
+        //Predicate p3 = (int i) -> i%2==0;
+        Predicate p4 = (var i) -> (Integer)i%2==0;
+        
+    }
+}
