f1224816b052b0b9f1beb047cc3544bec370cd0c
Ruben Dario <ruben.echeverri@axity.com>
2019-11-12 17:30:09 -0300

    dsjfndkjfbd

--- 1Z0-816/PracticeExam/Test4/Q48.java
+++ 1Z0-816/PracticeExam/Test4/Q48.java
@@ -1,8 +1,11 @@
 import java.util.stream.*;
+import java.util.*;
 
 public class Q48{
     public static void main(String... args){
-        Stream.of(0).findFirst().map(i -> null).orElseThrow(ArithmeticException::new);
+        //Stream.of(0).findFirst().map(i -> null).orElseThrow(ArithmeticException::new);
+        
+        List<?> l = new ArrayList<>();
     }
     
 }
